503f98aede551c910d1dd0f383fb27d103a962bb
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-10-27 11:45:36 -0600

    Use parse_bin_image to natively get binary values

--- xor_stdin.py
+++ xor_stdin.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 
 import sys
+import parse_bin_image
 
 def main():
-    pass
+    # usage ./xor_images.py /path/to/image1 /path/to/image2 [OPTIONS]
+    fp1 = sys.argv[1]
+    fp2 = sys.argv[2]
+
+    pixel_size = "1"
+
+    for arg in sys.argv:
+        if "--pixel_size" in arg:
+            pixel_size = arg.split("=")[-1]
+            continue
+
+    ps = int(pixel_size)
+    parse_bin_image.parse_binary_image(fp1, pixel_size=ps);
+    parse_bin_image.parse_binary_image(fp2, pixel_size=ps);
 
 if __name__ == "__main__":
     main()
