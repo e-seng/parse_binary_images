a209e2021a4b2fc4b285fd090655eb5d2f250849
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-10-27 10:38:35 -0600

    Add parse_bin_image.py

---
+++ parse_bin_image.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import sys
+import os
+
+def main():
+    # usage: ./parse_bin_image /path/to/image [OPTIONS]
+
+def parse_binary_image(filepath, pixel_size=1):
+    image = Image.open(filepath)
+
+if __name__ == "__main__":
+    main()
